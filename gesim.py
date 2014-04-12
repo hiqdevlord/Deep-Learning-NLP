a2fc5e281f766b4e88163ec62dcd1b4a146cb636
hiqdevlord <hiqdevlord@gmail.com>
2014-04-13 00:30:24 +0800

    對文章作切詞取得token。

--- gesim.py
+++ gesim.py
@@ -1,4 +1,6 @@
 from gensim import corpora, models, similarities
+import logging
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 corpus = [[(0, 1.0), (1, 1.0), (2, 1.0)],
            [(2, 1.0), (3, 1.0), (4, 1.0), (5, 1.0), (6, 1.0), (8, 1.0)],
@@ -18,4 +20,29 @@ print(tfidf[vec])
 
 index = similarities.SparseMatrixSimilarity(tfidf[corpus], num_features=12)
 sims = index[tfidf[vec]]
-print(list(enumerate(sims)))
+print(list(enumerate(sims)));
+
+
+
+# From Strings to Vectors
+documents = ["Human machine interface for lab abc computer applications",
+             "A survey of user opinion of computer system response time",
+             "The EPS user interface management system",
+              "System and human system engineering testing of EPS",
+              "Relation of user perceived response time to error measurement",
+              "The generation of random binary unordered trees",
+              "The intersection graph of paths in trees",
+              "Graph minors IV Widths of trees and well quasi ordering",
+              "Graph minors A survey"];
+
+stoplist = set('for a of the and to in'.split())
+
+
+texts = [[word for word in document.lower().split() if word not in stoplist] for document in documents]
+
+
+all_tokens = sum(texts, [])
+tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
+texts = [[word for word in text if word not in tokens_once] for text in texts]
+
+print(texts);
